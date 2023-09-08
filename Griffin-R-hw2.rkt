0b6d600c1b84f0e634e72d261bd6c58b3ac37324
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-09-08 13:16:44 -0400

    update hw2

--- Griffin-R-hw2.rkt
+++ Griffin-R-hw2.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Griffin-R-hw2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 1 )
+
 ; make-roadracer: Number Number String Number -> roadracer
 (define-struct roadracer (rate max-psi frame wheelsize))
 
